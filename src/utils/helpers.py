d9b5185b1d611ef573f344b23bc0121ac9feb08a
“Eric <ewalzthony@gmail.com>
2022-06-23 21:49:46 +0200

    cleaning helpers

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,15 +1,27 @@
-import pandas
+import pandas as pd 
 
-def reading(path):
-    
-    df = pandas.read_csv(path)
+def load_dataframe(path:str) -> pd.DataFrame:
+    """Function to load the Adjacency Matrix from a csv file."""
+    df = pd.read_csv(path)
     #take the first row and put as columns header in df
-    header = df.iloc[0]
-    df = df[1: ]
-    df.columns =header
-    #
+    header, df = df.iloc[0], df[1: ]
+    ## the remaining of the data 
+    
+    ## make the header as as columns 
+    df.columns = header
+    # set the to processes 
     df = df.set_index('Processes')
-
- 
-
     return df 
+
+
+def get_edge_list(df:pd.DataFrame) -> list:
+    """Function to create a list of edges from a dataframe."""
+    return [(process, material) for process in df.index for material in df.columns]
+
+def get_edge_list_weighted(df:pd.DataFrame) -> list:
+    """Function to create a list of edges from a dataframe."""
+    return [(process, material, df.loc[process, material]) for process in df.index for material in df.columns]
+
+def clean_dataframe(df:pd.DataFrame) -> pd.DataFrame:
+    """Function to clean the dataframe."""
+    pass 
