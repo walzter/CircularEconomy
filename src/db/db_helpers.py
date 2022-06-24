583ac80be8c5870e4ad65051a06eed65a78efcd2
Ejbejaranos <91475734+EjbejaranosAI@users.noreply.github.com>
2022-06-24 22:01:32 +0200

    Creatin queries to remove and get a specific material

--- src/db/db_helpers.py
+++ src/db/db_helpers.py
@@ -21,3 +21,43 @@ def clear_db():
     return ele
 
 
+def get_node_count():
+    """
+    Get the number of nodes in the database.
+    """
+    ele = db.cypher_query("MATCH (n) RETURN count(n)")
+    return ele[0][0]
+
+
+def get_processes():
+    """
+    Get the processes in the database.
+    """
+    ele = db.cypher_query("MATCH (n:Process) RETURN n")
+    return ele
+
+def get_materials():
+    """
+    Get the materials in the database.
+    """
+    ele = db.cypher_query("MATCH (n:Material) RETURN n")
+    return ele
+
+# get a specific material from the database
+def get_material(name:str):
+    """
+    Get a specific material from the database.
+    """
+    ele = db.cypher_query("MATCH (n:Material {name: '" + name + "'}) RETURN n")
+    return ele
+
+    
+# remove a specific material from the database
+def remove_material(name:str):
+    """
+    Remove a specific material from the database.
+    """
+    ele = db.cypher_query("MATCH (n:Material {name: '" + name + "'}) DETACH DELETE n")
+    return ele
+
+
